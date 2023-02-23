f5f9f14896c452ea7be0b0aa6a96a026ffa680d5
Edward <github@edwardcs.com>
2023-02-23 20:47:58 +0200

    Implement receiveAttack method in Gameboard

--- src/__tests__/gameboardFactory.test.ts
+++ src/__tests__/gameboardFactory.test.ts
@@ -24,4 +24,45 @@ describe("Gameboard", () => {
     gameboard.placeShip(submarine, 0, 0, "horizontal");
     expect(() => gameboard.placeShip(cruiser, 0, 0, "horizontal")).toThrow();
   });
+
+  it("should hit the correct ship", () => {
+    const gameboard = Gameboard();
+    const cruiser = Ship(3, "Cruiser");
+    gameboard.placeShip(cruiser, 0, 0, "horizontal");
+    gameboard.receiveAttack(0, 0);
+    expect(cruiser.getHits()).toBe(1);
+  });
+
+  it("should not hit the ship if no ship is placed at coordinates", () => {
+    const gameboard = Gameboard();
+    const cruiser = Ship(3, "Cruiser");
+    gameboard.placeShip(cruiser, 0, 0, "horizontal");
+    gameboard.receiveAttack(5, 5);
+    expect(cruiser.getHits()).toBe(0);
+  });
+
+  it("should not hit the ship if already hit at specific coordinates", () => {
+    const gameboard = Gameboard();
+    const cruiser = Ship(3, "Cruiser");
+    gameboard.placeShip(cruiser, 0, 0, "horizontal");
+    gameboard.receiveAttack(0, 0);
+    gameboard.receiveAttack(0, 0);
+    expect(cruiser.getHits()).toBe(1);
+  });
+
+  it("should record hit at coordinates", () => {
+    const gameboard = Gameboard();
+    const cruiser = Ship(3, "Cruiser");
+    gameboard.placeShip(cruiser, 0, 0, "horizontal");
+    gameboard.receiveAttack(0, 0);
+    expect(gameboard.board[0][0]).toBe("hit");
+  });
+
+  it("should record miss at coordinates", () => {
+    const gameboard = Gameboard();
+    const cruiser = Ship(3, "Cruiser");
+    gameboard.placeShip(cruiser, 0, 0, "horizontal");
+    gameboard.receiveAttack(5, 5);
+    expect(gameboard.board[5][5]).toBe("miss");
+  });
 });
--- src/scripts/gameboardFactory.ts
+++ src/scripts/gameboardFactory.ts
@@ -4,9 +4,20 @@ function Gameboard() {
   // Create board of 10x10
   const board = Array(10).fill(Array(10).fill(null));
 
-  // Check if ship is placed at coordinates
   function isShipPlaced(x: number, y: number) {
     return board[x][y] !== null;
+  }
+
+  function markHit(x: number, y: number) {
+    board[x][y] = "hit";
+  }
+
+  function markMiss(x: number, y: number) {
+    board[x][y] = "miss";
+  }
+
+  function isHit(x: number, y: number) {
+    return board[x][y] === "hit";
   }
 
   // Place ship at coordinates by using Ship factory
@@ -26,9 +37,28 @@ function Gameboard() {
     }
   }
 
+  // Receive attack at specified coordinates
+  function receiveAttack(x: number, y: number) {
+    // If ship is already hit at coordinates, return
+    if (isHit(x, y)) return;
+
+    // If no ship is placed at coordinates, mark as miss
+    if (!isShipPlaced(x, y)) {
+      markMiss(x, y);
+      return;
+    }
+
+    // Check if ship is placed at coordinates
+    if (isShipPlaced(x, y)) {
+      board[x][y].hit();
+      markHit(x, y);
+    }
+  }
+
   return {
     board,
     placeShip,
+    receiveAttack,
   };
 }
 
