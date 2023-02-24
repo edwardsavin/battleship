95427cd8f9d1dbd37db20d7894ad4373e3c74c28
Edward <github@edwardcs.com>
2023-02-24 22:53:47 +0200

    Add checks for ship placement

--- src/components/setupPlayerShips-block.ts
+++ src/components/setupPlayerShips-block.ts
@@ -1,6 +1,7 @@
 import createGameBlock from "./game-block";
 import renderBoard from "./renderBoard";
 import Game from "../scripts/gameFactory";
+import { ShipType } from "../scripts/shipFactory";
 
 function removeSetupNewGameBlock() {
   const newGameContainer = document.querySelector(
@@ -9,6 +10,60 @@ function removeSetupNewGameBlock() {
   newGameContainer?.remove();
 }
 
+function isOutOfBounds(
+  x: number,
+  y: number,
+  length: number,
+  isHorizontal: boolean
+): boolean {
+  return isHorizontal ? y + length > 10 : x + length > 10;
+}
+
+function isCellOccupied(target: HTMLDivElement): boolean {
+  return target.classList.contains("ship");
+}
+
+function isPlacementValid(target: HTMLDivElement): boolean {
+  return target.dataset.validPlacement === "true";
+}
+
+function validateShipPlacement(
+  tempBoard: HTMLDivElement,
+  shipToPlace: ShipType,
+  isHorizontal: boolean,
+  target: HTMLDivElement,
+  x: number,
+  y: number
+) {
+  const updatedTarget = target;
+
+  // Check if the placement is out of bounds
+  if (isOutOfBounds(x, y, shipToPlace.length, isHorizontal)) {
+    updatedTarget.dataset.validPlacement = "false";
+    return;
+  }
+
+  // Check if any of the adjacent cells are occupied
+  for (let i = 0; i < shipToPlace.length; i += 1) {
+    const adjacentCell = isHorizontal
+      ? (tempBoard.querySelector(
+          `[data-x="${x}"][data-y="${y + i}"]`
+        ) as HTMLDivElement)
+      : (tempBoard.querySelector(
+          `[data-x="${x + i}"][data-y="${y}"]`
+        ) as HTMLDivElement);
+
+    // If the adjacent cell is occupied, the placement is invalid
+    if (adjacentCell && adjacentCell.classList.contains("ship")) {
+      updatedTarget.dataset.validPlacement = "false";
+      return;
+    }
+  }
+
+  updatedTarget.dataset.validPlacement = "true";
+}
+
+// Allow player to place their ships on the board
 function setupPlayerShips(playerName: string) {
   removeSetupNewGameBlock();
 
@@ -19,9 +74,16 @@ function setupPlayerShips(playerName: string) {
   const tempBoardTitle = document.createElement("h2");
   tempBoardTitle.classList.add("temp-board__title");
 
+  let isHorizontal = true;
   const rotateButton = document.createElement("button");
   rotateButton.classList.add("temp-board__rotate-button");
-  rotateButton.textContent = "Rotate";
+
+  rotateButton.textContent = "Rotate to Horizontal";
+  if (isHorizontal) {
+    rotateButton.textContent = "Rotate to Vertical";
+  } else {
+    rotateButton.textContent = "Rotate to Horizontal";
+  }
 
   const tempBoard = renderBoard(playerName);
   tempBoard.classList.add("board-temp");
@@ -36,9 +98,26 @@ function setupPlayerShips(playerName: string) {
   let shipToPlace = game.player.ships[0];
   tempBoardTitle.textContent = `Place your ${shipToPlace.name}`;
 
-  let isHorizontal = true;
   rotateButton.addEventListener("click", () => {
     isHorizontal = !isHorizontal;
+    if (isHorizontal) {
+      rotateButton.textContent = "Rotate to Vertical";
+    } else {
+      rotateButton.textContent = "Rotate to Horizontal";
+    }
+  });
+
+  tempBoard.addEventListener("mouseover", (e) => {
+    const target = e.target as HTMLDivElement;
+    const x = Number(target.dataset.x);
+    const y = Number(target.dataset.y);
+
+    validateShipPlacement(tempBoard, shipToPlace, isHorizontal, target, x, y);
+  });
+
+  tempBoard.addEventListener("mouseout", (e) => {
+    const target = e.target as HTMLDivElement;
+    delete target.dataset.validPlacement;
   });
 
   // Add event listener to each cell in the temporary board
@@ -47,7 +126,22 @@ function setupPlayerShips(playerName: string) {
     const x = Number(target.dataset.x);
     const y = Number(target.dataset.y);
 
-    // Check if the cell is already occupied
+    if (isOutOfBounds(x, y, shipToPlace.length, isHorizontal)) {
+      alert("Ship is out of bounds");
+      return;
+    }
+
+    if (isCellOccupied(target)) {
+      alert("This cell is already occupied by a ship");
+      return;
+    }
+
+    if (!isPlacementValid(target)) {
+      alert("Cannot place ship on top of another ship");
+      return;
+    }
+
+    // Add the ship to the game board
     try {
       game.player.placeShip(shipToPlace, x, y, isHorizontal);
     } catch (error) {
@@ -56,24 +150,27 @@ function setupPlayerShips(playerName: string) {
     }
 
     // Add the ship class to the cell
-    target.classList.add("board__cell--ship");
-    if (isHorizontal) {
-      for (let i = 1; i < shipToPlace.length; i += 1) {
-        const cell = document.querySelector(
-          `.board-temp[data-x="${x + i}"][data-y="${y}"]`
-        );
-        cell?.classList.add("board__cell--ship");
-      }
-    } else {
-      for (let i = 1; i < shipToPlace.length; i += 1) {
-        const cell = document.querySelector(
-          `.board-temp[data-x="${x}"][data-y="${y + i}"]`
-        );
-        cell?.classList.add("board__cell--ship");
-      }
+    target.classList.add("ship");
+
+    // Add data attributes to the cell with the part of the ship
+    target.dataset.ship = shipToPlace.name;
+    target.dataset.shipPart = "0";
+
+    // Add the ship class and data attributes to the adjacent cells
+    for (let i = 1; i < shipToPlace.length; i += 1) {
+      const adjacentCell = tempBoard.querySelector(
+        `[data-x="${isHorizontal ? x : x + i}"][data-y="${
+          isHorizontal ? y + i : y
+        }"]`
+      ) as HTMLDivElement;
+
+      adjacentCell.classList.add("ship");
+      adjacentCell.dataset.ship = shipToPlace.name;
+      adjacentCell.dataset.shipPart = `${i}`;
     }
 
-    // Check if there are any more ships to place
+    // Check if there are any more ships to place and place the next ship
+    // If there aren't, remove the temporary board and create the game board
     const nextShipIndex = game.player.ships.indexOf(shipToPlace) + 1;
     if (nextShipIndex < game.player.ships.length) {
       shipToPlace = game.player.ships[nextShipIndex];
