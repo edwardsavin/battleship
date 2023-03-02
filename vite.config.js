a303f8b54f6bd873517c320bc8ca382305ac09a0
Edward <github@edwardcs.com>
2023-03-02 15:07:30 +0200

    Update favicons with vite-plugin-favicons-inject

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-export default {
+import { defineConfig } from "vite";
+import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
+
+export default defineConfig({
   base: "/battleship/",
-};
+  plugins: [vitePluginFaviconsInject("./src/assets/favicon.png")],
+});
